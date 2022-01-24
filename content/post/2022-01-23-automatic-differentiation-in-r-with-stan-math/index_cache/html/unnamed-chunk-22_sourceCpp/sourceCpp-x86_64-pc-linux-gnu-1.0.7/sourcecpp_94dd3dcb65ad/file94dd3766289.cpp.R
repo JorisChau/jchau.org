92ca367f80dd4c6eda05a649ee13429df2f1a3e3
JorisChau <joris.chau@openanalytics.eu>
2022-01-24 12:37:31 +0100

    edit autodiff post

--- content/post/2022-01-23-automatic-differentiation-in-r-with-stan-math/index_cache/html/unnamed-chunk-22_sourceCpp/sourceCpp-x86_64-pc-linux-gnu-1.0.7/sourcecpp_94dd3dcb65ad/file94dd3766289.cpp.R
+++
@@ -1,5 +0,0 @@
-`.sourceCpp_9_DLLInfo` <- dyn.load('/home/jchau/git/jchau.org/content/post/2022-01-23-automatic-differentiation-in-r-with-stan-math/index_cache/html/unnamed-chunk-22_sourceCpp/sourceCpp-x86_64-pc-linux-gnu-1.0.7/sourcecpp_94dd3dcb65ad/sourceCpp_10.so')
-
-fjac_watson <- Rcpp:::sourceCppFunction(function(theta, nms) {}, FALSE, `.sourceCpp_9_DLLInfo`, 'sourceCpp_9_fjac_watson')
-
-rm(`.sourceCpp_9_DLLInfo`)
